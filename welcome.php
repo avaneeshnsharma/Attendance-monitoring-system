66a63121c321e0b680dba9e30352a561f34852a8
avaneeshnsharma <154623092+avaneeshnsharma@users.noreply.github.com>
2023-12-22 23:22:49 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['loggedin']) ||$_SESSION['loggedin']!=true){
+header("location: login.php");
+exit;
+
+}
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome <?php $_SESSION['username']?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php require 'nav.php' ?>
+
+ 
+
+  <div class="container my-4">
+  <div class="alert alert-success" role="alert">
+  <h4 class="alert-heading"> Welcome <?php echo $_SESSION["username"];?></h4>
+  <p>Hey how are you you are logged in as  <?php echo $_SESSION["username"];?>  Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+  <hr>
+  <p class="mb-0">Whenever you need to,be sure to logout using this link <a href="/loginf/logout.php">Logout madhi </a>.</p>
+</div>
+  </div>
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+  </body>
+</html>
