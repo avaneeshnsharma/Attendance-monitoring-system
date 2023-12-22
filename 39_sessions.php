dd8f6f6d515381d3922ec38fec4c8dc59288e013
avaneeshnsharma <154623092+avaneeshnsharma@users.noreply.github.com>
2023-12-22 23:32:21 +0530

    Add files via upload

---
+++ 39_sessions.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION['username'])){
+    echo "welcome".$_SESSION['username'];
+    echo "<br> fav category is ".$_SESSION['favcat'];
+    echo "<br>";
+
+
+}
+else{
+
+
+    echo "please login to continue";
+}
+
+
+?>
---
+++ 39logout.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+session_unset();
+session_destroy();
+
+echo "you have been logged out ";
+//session_start() session_unset() session_destroy() 
+
+?>
